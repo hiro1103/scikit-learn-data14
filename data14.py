a2b1b08a000272b4b763854d9c2c958da811ed46
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-19 09:34:58 +0900

    Add files via upload

--- data14.py
+++ data14.py
@@ -1,19 +1,70 @@
-from glob import glob1
-from sklearn import metrics
-from mlxtend.plotting import plot_decision_regions
 import tensorflow as tf
 import numpy as np
-import warnings
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
 import sklearn.model_selection
-import sklearn.model_selection
-warnings.simplefilter('ignore')
+from mlxtend.plotting import plot_decision_regions
+
+g = tf.Graph()
+with g.as_default():
+    a = tf.constant(1, name='a')
+    b = tf.constant(2, name='b')
+    c = tf.constant(3, name='c')
+    z = 2*(a - b) + c
+
+with tf.compat.v1.Session(graph=g) as sess:
+    print('Result: z =', sess.run(z))
+    print('Result: z =', z.eval())
+
+
+a = tf.constant(1, name='a')
+b = tf.constant(2, name='b')
+c = tf.constant(3, name='c')
+
+z = 2*(a - b) + c
+tf.print('Result: z =', z)
+
+g = tf.Graph()
+with g.as_default():
+    a = tf.compat.v1.placeholder(shape=None, dtype=tf.int32, name='tf_a')
+    b = tf.compat.v1.placeholder(shape=None, dtype=tf.int32, name='tf_b')
+    c = tf.compat.v1.placeholder(shape=None, dtype=tf.int32, name='tf_c')
+    z = 2*(a - b) + c
+
+with tf.compat.v1.Session(graph=g) as sess:
+    feed_dict = {a: 1, b: 2, c: 3}
+    print('Result: z =', sess.run(z, feed_dict=feed_dict))
+
+
+def compute_z(a, b, c):
+    r1 = tf.subtract(a, b)
+    r2 = tf.multiply(2, r1)
+    z = tf.add(r2, c)
+    return z
+
+
+tf.print('Scalar Inputs:', compute_z(1, 2, 3))
+tf.print('Rank 1 Inputs:', compute_z([1], [2], [3]))
+tf.print('Rank 2 Inputs:', compute_z([[1]], [[2]], [[3]]))
+
+
+@tf.function
+def compute_z(a, b, c):
+    r1 = tf.subtract(a, b)
+    r2 = tf.multiply(2, r1)
+    z = tf.add(r2, c)
+    return z
+
+
+tf.print('Scalar Inputs:', compute_z(1, 2, 3))
+tf.print('Rank 1 Inputs:', compute_z([1], [2], [3]))
+tf.print('Rank 2 Inputs:', compute_z([[1]], [[2]], [[3]]))
 
 
 @tf.function(input_signature=(tf.TensorSpec(shape=[None], dtype=tf.int32),
                               tf.TensorSpec(shape=[None], dtype=tf.int32),
-                              tf.TensorSpec(shape=[None], dtype=tf.int32)))
+                              tf.TensorSpec(shape=[None], dtype=tf.int32),))
 def compute_z(a, b, c):
     r1 = tf.subtract(a, b)
     r2 = tf.multiply(2, r1)
@@ -24,9 +75,39 @@ def compute_z(a, b, c):
 tf.print('Rank 1 Inputs:', compute_z([1], [2], [3]))
 tf.print('Rank 1 Inputs:', compute_z([1, 2], [2, 4], [3, 6]))
 
+
+tf.TensorSpec(shape=[None], dtype=tf.int32)
+
+
+a = tf.Variable(initial_value=3.14, name='var_a')
+b = tf.Variable(initial_value=[1, 2, 3], name='var_b')
+c = tf.Variable(initial_value=[True, False], dtype=tf.bool)
+d = tf.Variable(initial_value=['abc'], dtype=tf.string)
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+a.trainable
+
+
+w = tf.Variable([1, 2, 3], trainable=False)
+
+print(w.trainable)
+
+
+print(w.assign([3, 1, 4], read_value=True))
+w.assign_add([2, -1, 2], read_value=False)
+
+print(w.value())
+
+
 tf.random.set_seed(1)
 init = tf.keras.initializers.GlorotNormal()
+
 tf.print(init(shape=(3,)))
+
 
 v = tf.Variable(init(shape=(2, 3)))
 tf.print(v)
@@ -40,8 +121,10 @@ class MyModule(tf.Module):
 
 
 m = MyModule()
-print('All module variables:', [v.shape for v in m.variables])
-print('Trainable variable:', [v.shape for v in m.trainable_variables])
+print('All module variables: ', [v.shape for v in m.variables])
+print('Trainable variable:   ', [v.shape for v in
+                                 m.trainable_variables])
+
 
 tf.random.set_seed(1)
 w = tf.Variable(tf.random.uniform((3, 3)))
@@ -61,80 +144,123 @@ print(w.trainable, b.trainable)
 
 x = tf.convert_to_tensor([1.4])
 y = tf.convert_to_tensor([2.1])
+
+with tf.GradientTape() as tape:
+    z = tf.add(tf.multiply(w, x), b)
+    loss = tf.reduce_sum(tf.square(y - z))
+
+dloss_dw = tape.gradient(loss, w)
+
+tf.print('dL/dw : ', dloss_dw)
+
+tf.print(2*x * ((w*x + b) - y))
+
+with tf.GradientTape() as tape:
+    tape.watch(x)
+    z = tf.add(tf.multiply(w, x), b)
+    loss = tf.square(y - z)
+
+dloss_dx = tape.gradient(loss, x)
+
+tf.print('dL/dx:', dloss_dx)
+
+
+tf.print(2*w * ((w*x + b) - y))
+
 with tf.GradientTape(persistent=True) as tape:
     z = tf.add(tf.multiply(w, x), b)
-    loss = tf.reduce_sum(tf.square(y-z))
+    loss = tf.reduce_sum(tf.square(y - z))
 
-dloww_dw = tape.gradient(loss, w)
-tf.print('dL/dw:', dloww_dw)
+dloss_dw = tape.gradient(loss, w)
 dloss_db = tape.gradient(loss, b)
+
+tf.print('dL/dw:', dloss_dw)
 tf.print('dL/db:', dloss_db)
-# 計算した勾配を検証
-tf.print(2*x*(w*x+b-y))
 
 optimizer = tf.keras.optimizers.SGD()
-optimizer.apply_gradients(zip([dloww_dw, dloss_db], [w, b]))
+
+optimizer.apply_gradients(zip([dloss_dw, dloss_db], [w, b]))
+
 tf.print('Updated w:', w)
 tf.print('Updated bias:', b)
+
 
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(units=16, activation='relu'))
 model.add(tf.keras.layers.Dense(units=32, activation='relu'))
-# 遅延変数作成
+
 model.build(input_shape=(None, 4))
 model.summary()
 
-# モデルの変数を出力
 for v in model.variables:
     print('{:20s}'.format(v.name), v.trainable, v.shape)
 
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(
-    units=16,
-    activation=tf.keras.activations.relu,
-    kernel_initializer=tf.keras.initializers.glorot_uniform(),
-    bias_initializer=tf.keras.initializers.Constant(2.0)
-))
 
-model.add(tf.keras.layers.Dense(
-    units=32,
-    activation=tf.keras.activations.sigmoid,
-    kernel_initializer=tf.keras.regularizers.l1
-))
+model.add(
+    tf.keras.layers.Dense(
+        units=16,
+        activation=tf.keras.activations.relu,
+        kernel_initializer=tf.keras.initializers.GlorotNormal(),
+        bias_initializer=tf.keras.initializers.Constant(2.0)
+    ))
 
-model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
-              loss=tf.keras.losses.BinaryCrossentropy(),
-              metrics=[tf.keras.metrics.Accuracy(),
-                       tf.keras.metrics.Precision(),
-                       tf.keras.metrics.Recall()])
+model.add(
+    tf.keras.layers.Dense(
+        units=32,
+        activation=tf.keras.activations.sigmoid,
+        kernel_regularizer=tf.keras.regularizers.l1
+    ))
+
+model.build(input_shape=(None, 4))
+model.summary()
+
+model.compile(
+    optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[tf.keras.metrics.Accuracy(),
+             tf.keras.metrics.Precision(),
+             tf.keras.metrics.Recall(), ])
 
 tf.random.set_seed(1)
 np.random.seed(1)
+
 x = np.random.uniform(low=-1, high=1, size=(200, 2))
 y = np.ones(len(x))
 y[x[:, 0] * x[:, 1] < 0] = 0
+
 x_train = x[:100, :]
 y_train = y[:100]
 x_valid = x[100:, :]
 y_valid = y[100:]
+
 fig = plt.figure(figsize=(6, 6))
-plt.plot(x[y == 0, 0], x[y == 0, 1], 'o', alpha=0.75, markersize=10)
-plt.plot(x[y == 1, 0], x[y == 1, 1], '<', alpha=0.75, markersize=10)
+plt.plot(x[y == 0, 0],
+         x[y == 0, 1], 'o', alpha=0.75, markersize=10)
+plt.plot(x[y == 1, 0],
+         x[y == 1, 1], '<', alpha=0.75, markersize=10)
 plt.xlabel(r'$x_1$', size=15)
 plt.ylabel(r'$x_2$', size=15)
 plt.show()
 
+
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(
-    units=1, input_shape=(2,), activation='sigmoid'))
+model.add(tf.keras.layers.Dense(units=1,
+                                input_shape=(2,),
+                                activation='sigmoid'))
+
 model.summary()
+
 
 model.compile(optimizer=tf.keras.optimizers.SGD(),
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=[tf.keras.metrics.BinaryAccuracy()])
 
-hist = model.fit(x_train, y_train, validation_data=(x_valid, y_valid),
+
+hist = model.fit(x_train, y_train,
+                 validation_data=(x_valid, y_valid),
                  epochs=200, batch_size=2, verbose=0)
+
 
 history = hist.history
 
@@ -201,23 +327,23 @@ ax.yaxis.set_label_coords(-0.025, 1)
 plt.show()
 
 tf.random.set_seed(1)
-# 入力層
+
 inputs = tf.keras.Input(shape=(2,))
-# 隠れ層
+
 h1 = tf.keras.layers.Dense(units=4, activation='relu')(inputs)
 h2 = tf.keras.layers.Dense(units=4, activation='relu')(h1)
 h3 = tf.keras.layers.Dense(units=4, activation='relu')(h2)
-# 出力層
+
 outputs = tf.keras.layers.Dense(units=1, activation='sigmoid')(h3)
-# モデルを構築
+
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
 model.summary()
-# コンパイル
+
 model.compile(optimizer=tf.keras.optimizers.SGD(),
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=[tf.keras.metrics.BinaryAccuracy()])
-# 訓練
+
 hist = model.fit(x_train, y_train,
                  validation_data=(x_valid, y_valid),
                  epochs=200, batch_size=2, verbose=0)
@@ -263,19 +389,23 @@ class MyModel(tf.keras.Model):
 
 tf.random.set_seed(1)
 
+# testing:
 model = MyModel()
 model.build(input_shape=(None, 2))
 
 model.summary()
+
+# compile:
 model.compile(optimizer=tf.keras.optimizers.SGD(),
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=[tf.keras.metrics.BinaryAccuracy()])
 
-
+# train:
 hist = model.fit(x_train, y_train,
                  validation_data=(x_valid, y_valid),
                  epochs=200, batch_size=2, verbose=0)
 
+# Plotting
 history = hist.history
 
 fig = plt.figure(figsize=(16, 4))
@@ -298,29 +428,187 @@ ax.set_ylabel(r'$x_2$', size=15)
 ax.yaxis.set_label_coords(-0.025, 1)
 plt.show()
 
-dataset_path = tf.keras.utils.get_file(
-    "auto-mpg.data",
-    ("http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
-)
-column_name = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
-               'Acceleration', 'ModeYear', 'Origin']
-df = pd.read_csv(dataset_path, names=column_name, na_values='?', comment='\t',
-                 sep=' ', skipinitialspace=True)
 
-# NA行を削除
+class NoisyLinear(tf.keras.layers.Layer):
+    def __init__(self, output_dim, noise_stddev=0.1, **kwargs):
+        self.output_dim = output_dim
+        self.noise_stddev = noise_stddev
+        super(NoisyLinear, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.w = self.add_weight(name='weights',
+                                 shape=(input_shape[1], self.output_dim),
+                                 initializer='random_normal',
+                                 trainable=True)
+
+        self.b = self.add_weight(shape=(self.output_dim,),
+                                 initializer='zeros',
+                                 trainable=True)
+
+    def call(self, inputs, training=False):
+        if training:
+            batch = tf.shape(inputs)[0]
+            dim = tf.shape(inputs)[1]
+            noise = tf.random.normal(shape=(batch, dim),
+                                     mean=0.0,
+                                     stddev=self.noise_stddev)
+
+            noisy_inputs = tf.add(inputs, noise)
+        else:
+            noisy_inputs = inputs
+        z = tf.matmul(noisy_inputs, self.w) + self.b
+        return tf.keras.activations.relu(z)
+
+    def get_config(self):
+        config = super(NoisyLinear, self).get_config()
+        config.update({'output_dim': self.output_dim,
+                       'noise_stddev': self.noise_stddev})
+        return config
+
+
+tf.random.set_seed(1)
+
+noisy_layer = NoisyLinear(4)
+noisy_layer.build(input_shape=(None, 4))
+
+x = tf.zeros(shape=(1, 4))
+tf.print(noisy_layer(x, training=True))
+
+config = noisy_layer.get_config()
+new_layer = NoisyLinear.from_config(config)
+tf.print(new_layer(x, training=True))
+
+
+tf.random.set_seed(1)
+
+model = tf.keras.Sequential([
+    NoisyLinear(4, noise_stddev=0.1),
+    tf.keras.layers.Dense(units=4, activation='relu'),
+    tf.keras.layers.Dense(units=4, activation='relu'),
+    tf.keras.layers.Dense(units=1, activation='sigmoid')])
+
+model.build(input_shape=(None, 2))
+model.summary()
+
+# compile:
+model.compile(optimizer=tf.keras.optimizers.SGD(),
+              loss=tf.keras.losses.BinaryCrossentropy(),
+              metrics=[tf.keras.metrics.BinaryAccuracy()])
+
+# train:
+hist = model.fit(x_train, y_train,
+                 validation_data=(x_valid, y_valid),
+                 epochs=200, batch_size=2,
+                 verbose=0)
+
+history = hist.history
+
+fig = plt.figure(figsize=(16, 4))
+ax = fig.add_subplot(1, 3, 1)
+plt.plot(history['loss'], lw=4)
+plt.plot(history['val_loss'], lw=4)
+ax.set_xlabel('Epochs', size=15)
+
+ax = fig.add_subplot(1, 3, 2)
+plt.plot(history['binary_accuracy'], lw=4)
+plt.plot(history['val_binary_accuracy'], lw=4)
+ax.set_xlabel('Epochs', size=15)
+
+ax = fig.add_subplot(1, 3, 3)
+plot_decision_regions(X=x_valid, y=y_valid.astype(np.integer),
+                      clf=model)
+ax.set_xlabel(r'$x_1$', size=15)
+ax.xaxis.set_label_coords(1, -0.025)
+ax.set_ylabel(r'$x_2$', size=15)
+ax.yaxis.set_label_coords(-0.025, 1)
+plt.show()
+tf.random.set_seed(1)
+np.random.seed(1)
+dataset_path = tf.keras.utils.get_file("auto-mpg.data",
+                                       ("http://archive.ics.uci.edu/ml/machine-learning-databases"
+                                        "/auto-mpg/auto-mpg.data"))
+
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower',
+                'Weight', 'Acceleration', 'ModelYear', 'Origin']
+
+df = pd.read_csv(dataset_path, names=column_names,
+                 na_values="?", comment='\t',
+                 sep=" ", skipinitialspace=True)
+
+df.tail()
+
+
+print(df.isna().sum())
+
 df = df.dropna()
 df = df.reset_index(drop=True)
-# 訓練データセットとテストデータセットに分割
+df.tail()
+
+
 df_train, df_test = sklearn.model_selection.train_test_split(
     df, train_size=0.8)
 train_stats = df_train.describe().transpose()
-numeric_column_names = ['Cylinders', 'Displacement', 'Horsepower', 'Weight',
-                        'Acceleration']
+train_stats
+
+
+numeric_column_names = ['Cylinders', 'Displacement',
+                        'Horsepower', 'Weight', 'Acceleration']
+
 df_train_norm, df_test_norm = df_train.copy(), df_test.copy()
+
 for col_name in numeric_column_names:
     mean = train_stats.loc[col_name, 'mean']
     std = train_stats.loc[col_name, 'std']
-    df_train_norm.loc[:, col_name] = (df_train_norm.loc[:, col_name]-mean)/std
-    df_test_norm.loc[:, col_name] = (df_test_norm.loc[:, col_name]-mean)/std
+    df_train_norm.loc[:, col_name] = (
+        df_train_norm.loc[:, col_name] - mean)/std
+    df_test_norm.loc[:, col_name] = (df_test_norm.loc[:, col_name] - mean)/std
 
-print(df_test_norm.tail())
+df_train_norm.tail()
+numeric_features = []
+
+for col_name in numeric_column_names:
+    numeric_features.append(tf.feature_column.numeric_column(key=col_name))
+
+numeric_features
+
+
+feature_year = tf.feature_column.numeric_column(key="ModelYear")
+
+bucketized_features = []
+
+bucketized_features.append(tf.feature_column.bucketized_column(
+    source_column=feature_year,
+    boundaries=[73, 76, 79]))
+
+print(bucketized_features)
+feature_origin = tf.feature_column.categorical_column_with_vocabulary_list(
+    key='Origin',
+    vocabulary_list=[1, 2, 3])
+
+categorical_indicator_features = []
+categorical_indicator_features.append(
+    tf.feature_column.indicator_column(feature_origin))
+
+print(categorical_indicator_features)
+
+
+def train_input_fn(df_train, batch_size=8):
+    df = df_train.copy()
+    train_x, train_y = df, df.pop('MPG')
+    dataset = tf.data.Dataset.from_tensor_slices((dict(train_x), train_y))
+
+    # shuffle, repeat, and batch the examples
+    return dataset.shuffle(1000).repeat().batch(batch_size)
+
+
+ds = train_input_fn(df_train_norm)
+batch = next(iter(ds))
+print('Keys:', batch[0].keys())
+print('Batch Model Years:', batch[0]['ModelYear'])
+
+
+all_feature_columns = (numeric_features +
+                       bucketized_features +
+                       categorical_indicator_features)
+
+print(all_feature_columns)
